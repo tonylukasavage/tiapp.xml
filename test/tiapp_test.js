e37a934c57ecfbca07b2c68d6f1b9076f2d2b772
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-04-09 15:36:59 -0400

    test cleanup

--- test/tiapp_test.js
+++ test/tiapp_test.js
@@ -1,17 +1,21 @@
-var should = require('should'),
+var path = require('path'),
+	should = require('should'),
 	Tiapp = require('..');
 
 var INVALID_TIAPP_ARGS = [123, function(){}, [1,2,3], true, false, NaN, Infinity, null],
-	VALID_TIAPP_ARGS = [undefined, './tiapp.xml'];
+	VALID_TIAPP_ARGS = [undefined, './tiapp.xml'],
+	TIAPP_XML = path.join('fixtures', 'tiapp.xml');
 
 describe('tiapp.xml.js', function() {
 
-	it('should create Tiapp with no arguments', function() {
-		(function() {
-			var tiapp = new Tiapp();
-			should.exist(tiapp.parse);
-			tiapp.parse.should.be.a.function;
-		}).should.not.throw();
+	it('should create Tiapp with no tiapp.xml', function() {
+		var tiapp = new Tiapp();
+		should.exist(tiapp.parse);
+		tiapp.parse.should.be.a.function;
+	});
+
+	it('should create Tiapp with explicit tiapp.xml', function() {
+		var tiapp = new Tiapp();
 	});
 
 	INVALID_TIAPP_ARGS.forEach(function(arg) {
