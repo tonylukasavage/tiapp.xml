9cba55be68ffe8b252d7cade2fcb23bfcb8e7399
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2015-01-12 17:57:24 +0000

    added uses-sdk and manifest attribsa

--- test/tiapp_test.js
+++ test/tiapp_test.js
@@ -568,11 +568,44 @@ describe('Tiapp', function() {
     describe('#Tiapp android', function() {
         it('should load android', function() {
             var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>paul ryan</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"><manifest android:versionCode="1" android:versionName="1.0.01"><uses-sdk android:minSdkVersion="10" android:targetSdkVersion="17"/><!-- Allows the API to download data from Google Map servers --><uses-permission android:name="android.permission.INTERNET"/><uses-permission android:name="android.permission.GET_ACCOUNTS"/><uses-permission android:name="android.permission.WAKE_LOCK"/><uses-permission android:name="com.google.android.c2dm.permission.RECEIVE"/><uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/><uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/><uses-permission android:name="android.permission.READ_PHONE_STATE"/><uses-permission android:name="android.permission.VIBRATE"/><uses-permission android:name="android.permission.READ_CALENDAR"/><uses-permission android:name="android.permission.WRITE_CALENDAR"/><uses-permission android:maxSdkVersion="18" android:name="android.permission.WRITE_EXTERNAL_STORAGE"/><permission android:name="com.example.test.permission.C2D_MESSAGE" android:protectionLevel="signature"/><uses-permission android:name="com.example.test.permission.C2D_MESSAGE"/><!-- Use GPS for device location --><uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/><!-- Use Wi-Fi or mobile connection for device location --><uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/><!-- Allows the API to access Google web-based services --><uses-permission android:name="com.google.android.providers.gsf.permission.READ_GSERVICES"/><!-- Specify OpenGL ES 2.0 as a requirement --><uses-feature android:glEsVersion="0x00020000" android:required="true"/><!-- Replace com.domain.appid with your application ID --><uses-permission android:name="com.example.test.permission.MAPS_RECEIVE"/><permission android:name="com.example.test.permission.MAPS_RECEIVE" android:protectionLevel="signature"/><application android:theme="@style/Theme.NoActionBar"><!-- Key for Android applications --><meta-data android:name="com.google.android.maps.v2.API_KEY" android:value="AIzaSyChq8dwoXYhzjV-CxGbgD9ZxICeCJb86-o"/></application></manifest></android></ti:app>');
-            var android = tiapp.getAndroidVersion();
+            var tmpFile = path.resolve('tmp', 'android.tiapp.xml');
             tiapp.id.should.equal('com.example.test');
-            should.exist(android);
-            android.versionCode.should.equal('1');
-            android.versionName.should.equal('1.0.01');
+            //as loaded
+            tiapp.android.versionCode.should.equal('1');
+            tiapp.android.versionName.should.equal('1.0.01');
+            tiapp.android.minSdkVersion.should.equal("10");
+            tiapp.android.targetSdkVersion.should.equal("17");
+            //as modified
+            tiapp.android.versionName = "1.0.2";
+            tiapp.android.versionCode = "2";
+            tiapp.android.versionCode.should.equal('2');
+            tiapp.android.versionName.should.equal('1.0.2');
+            tiapp.android.minSdkVersion = "14";
+            tiapp.android.targetSdkVersion = "19";
+            tiapp.android.minSdkVersion.should.equal("14");
+            tiapp.android.targetSdkVersion.should.equal("19");
+            tiapp.write(tmpFile);
+        });
+
+        it('should load not add versionCode and versionName if no manifest, but will be created on addition', function() {
+            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>paul ryan</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"></android></ti:app>');
+            var tmpFile = path.resolve('tmp', 'android.tiapp.xml');
+            tiapp.id.should.equal('com.example.test');
+            //as loaded
+            should.not.exist(tiapp.android.versionName);
+            should.not.exist(tiapp.android.versionCode);
+            should.not.exist(tiapp.android.minSdkVersion);
+            should.not.exist(tiapp.android.targetSdkVersion);
+            //as modified
+            tiapp.android.versionName = "1.0.2";
+            tiapp.android.versionCode = "2";
+            tiapp.android.minSdkVersion = "14";
+            tiapp.android.targetSdkVersion = "19";
+            tiapp.android.minSdkVersion.should.equal("14");
+            tiapp.android.targetSdkVersion.should.equal("19");
+            tiapp.android.versionCode.should.equal('2');
+            tiapp.android.versionName.should.equal('1.0.2');
+            //tiapp.write(tmpFile);
         });
     });
 
