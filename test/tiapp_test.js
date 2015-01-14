101aa68679951003308984de37e24b1cc9f5d88b
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2015-01-14 12:31:51 +0000

    set permissions

--- test/tiapp_test.js
+++ test/tiapp_test.js
@@ -567,7 +567,7 @@ describe('Tiapp', function() {
 
     describe('#Tiapp android', function() {
         it('should load android', function() {
-            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>paul ryan</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"><manifest android:versionCode="1" android:versionName="1.0.01"><uses-sdk android:minSdkVersion="10" android:targetSdkVersion="17"/><!-- Allows the API to download data from Google Map servers --><uses-permission android:name="android.permission.INTERNET"/><uses-permission android:name="android.permission.GET_ACCOUNTS"/><uses-permission android:name="android.permission.WAKE_LOCK"/><uses-permission android:name="com.google.android.c2dm.permission.RECEIVE"/><uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/><uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/><uses-permission android:name="android.permission.READ_PHONE_STATE"/><uses-permission android:name="android.permission.VIBRATE"/><uses-permission android:name="android.permission.READ_CALENDAR"/><uses-permission android:name="android.permission.WRITE_CALENDAR"/><uses-permission android:maxSdkVersion="18" android:name="android.permission.WRITE_EXTERNAL_STORAGE"/><permission android:name="com.example.test.permission.C2D_MESSAGE" android:protectionLevel="signature"/><uses-permission android:name="com.example.test.permission.C2D_MESSAGE"/><!-- Use GPS for device location --><uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/><!-- Use Wi-Fi or mobile connection for device location --><uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/><!-- Allows the API to access Google web-based services --><uses-permission android:name="com.google.android.providers.gsf.permission.READ_GSERVICES"/><!-- Specify OpenGL ES 2.0 as a requirement --><uses-feature android:glEsVersion="0x00020000" android:required="true"/><!-- Replace com.domain.appid with your application ID --><uses-permission android:name="com.example.test.permission.MAPS_RECEIVE"/><permission android:name="com.example.test.permission.MAPS_RECEIVE" android:protectionLevel="signature"/><application android:theme="@style/Theme.NoActionBar"><!-- Key for Android applications --><meta-data android:name="com.google.android.maps.v2.API_KEY" android:value="AIzaSyChq8dwoXYhzjV-CxGbgD9ZxICeCJb86-o"/></application></manifest></android></ti:app>');
+            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>appcelerator</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"><manifest android:versionCode="1" android:versionName="1.0.01"><uses-sdk android:minSdkVersion="10" android:targetSdkVersion="17"/><!-- Allows the API to download data from Google Map servers --><uses-permission android:name="android.permission.INTERNET"/><uses-permission android:name="android.permission.GET_ACCOUNTS"/><uses-permission android:name="android.permission.WAKE_LOCK"/><uses-permission android:name="com.google.android.c2dm.permission.RECEIVE"/><uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/><uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/><uses-permission android:name="android.permission.READ_PHONE_STATE"/><uses-permission android:name="android.permission.VIBRATE"/><uses-permission android:name="android.permission.READ_CALENDAR"/><uses-permission android:name="android.permission.WRITE_CALENDAR"/><uses-permission android:maxSdkVersion="18" android:name="android.permission.WRITE_EXTERNAL_STORAGE"/><permission android:name="com.example.test.permission.C2D_MESSAGE" android:protectionLevel="signature"/><uses-permission android:name="com.example.test.permission.C2D_MESSAGE"/><!-- Use GPS for device location --><uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/><!-- Use Wi-Fi or mobile connection for device location --><uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/><!-- Allows the API to access Google web-based services --><uses-permission android:name="com.google.android.providers.gsf.permission.READ_GSERVICES"/><!-- Specify OpenGL ES 2.0 as a requirement --><uses-feature android:glEsVersion="0x00020000" android:required="true"/><!-- Replace com.domain.appid with your application ID --><uses-permission android:name="com.example.test.permission.MAPS_RECEIVE"/><permission android:name="com.example.test.permission.MAPS_RECEIVE" android:protectionLevel="signature"/><application android:theme="@style/Theme.NoActionBar"><!-- Key for Android applications --><meta-data android:name="com.google.android.maps.v2.API_KEY" android:value="AIzaSyChq8dwoXYhzjV-CxGbgD9ZxICeCJb86-o"/></application></manifest></android></ti:app>');
             var tmpFile = path.resolve('tmp', 'android.tiapp.xml');
             tiapp.id.should.equal('com.example.test');
             //as loaded
@@ -588,7 +588,7 @@ describe('Tiapp', function() {
         });
 
         it('should not load properties if no manifest, but will on addition', function() {
-            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>paul ryan</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"></android></ti:app>');
+            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>appcelerator</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"></android></ti:app>');
             var tmpFile = path.resolve('tmp', 'android.tiapp.xml');
             tiapp.id.should.equal('com.example.test');
             //as loaded
@@ -610,7 +610,7 @@ describe('Tiapp', function() {
         });
 
         it('application settings', function() {
-            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>paul ryan</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"/></ti:app>');
+            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>appcelerator</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"/></ti:app>');
             var tmpFile = path.resolve('tmp', 'android.tiapp.xml');
             var permissions = tiapp.getAndroidUsesPermissions();
             tiapp.id.should.equal('com.example.test');
@@ -630,7 +630,7 @@ describe('Tiapp', function() {
         });
 
         it('android permissions and uses-permissions', function() {
-            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>paul ryan</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"><manifest><uses-permission android:name="android.permission.INTERNET"/><uses-permission android:name="android.permission.GET_ACCOUNTS"/><uses-permission android:name="android.permission.WAKE_LOCK"/><uses-permission android:name="com.google.android.c2dm.permission.RECEIVE"/><uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/><uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/><uses-permission android:name="android.permission.READ_PHONE_STATE"/><uses-permission android:name="android.permission.VIBRATE"/><uses-permission android:name="android.permission.READ_CALENDAR"/><uses-permission android:name="android.permission.WRITE_CALENDAR"/><uses-permission android:maxSdkVersion="18" android:name="android.permission.WRITE_EXTERNAL_STORAGE"/></manifest></android></ti:app>');
+            var tiapp = tiappXml.parse('<ti:app xmlns:ti="http://ti.appcelerator.org"><id>com.example.test</id><publisher>appcelerator</publisher><android xmlns:android="http://schemas.android.com/apk/res/android"><manifest><uses-permission android:name="android.permission.INTERNET"/><uses-permission android:name="android.permission.GET_ACCOUNTS"/><uses-permission android:name="android.permission.WAKE_LOCK"/><uses-permission android:name="com.google.android.c2dm.permission.RECEIVE"/><uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/><uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/><uses-permission android:name="android.permission.READ_PHONE_STATE"/><uses-permission android:name="android.permission.VIBRATE"/><uses-permission android:name="android.permission.READ_CALENDAR"/><uses-permission android:name="android.permission.WRITE_CALENDAR"/><uses-permission android:maxSdkVersion="18" android:name="android.permission.WRITE_EXTERNAL_STORAGE"/></manifest></android></ti:app>');
             var tmpFile = path.resolve('tmp', 'android.tiapp.xml');
             var permissions = tiapp.getAndroidUsesPermissions();
             //valid permissions
@@ -646,6 +646,21 @@ describe('Tiapp', function() {
             permissions.should.containEql('android.permission.READ_CALENDAR');
             permissions.should.containEql('com.google.android.c2dm.permission.RECEIVE');
             permissions.should.containEql('android.permission.WRITE_CALENDAR');
+            permissions.should.not.containEql('android.permission.CAMERA');
+            permissions.should.not.containEql('com.example.SOMECUSTOMPERMISSION');
+            tiapp.setAndroidPermissions({
+                'name' : 'android.permission.CAMERA'
+            });
+            tiapp.setAndroidPermissions({
+                'name' : 'com.example.SOMECUSTOMPERMISSION',
+                'maxSdkVersion' : '18'
+            });
+            permissions = tiapp.getAndroidUsesPermissions();
+            //updated permissions
+            permissions.should.have.length(13);
+            permissions.should.containEql('android.permission.CAMERA');
+            permissions.should.containEql('com.example.SOMECUSTOMPERMISSION');
+            tiapp.write(tmpFile);
         });
     });
 
